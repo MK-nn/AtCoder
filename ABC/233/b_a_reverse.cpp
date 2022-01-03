ba2b0900dceeb62f879cafa261479f42c353d3b0
MK-nn <okuken99@gmail.com>
2022-01-03 19:41:51 +0900

    solve b problem

---
+++ ABC/233/b_a_reverse.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  int l, r;
+  string s;
+  cin >> l >> r;
+  cin >> s;
+
+  string s_1 = s.substr(0, l - 1);
+  string s_2 = s.substr(l - 1, r - l + 1);
+  string s_3 = s.substr(r);
+  reverse(s_2.begin(), s_2.end());
+
+  cout << s_1 << s_2 << s_3 << endl;
+  // cout << "s1" << s_1 << endl;
+  // cout << "s2" << s_2 << endl;
+  // cout << "s3" << s_3 << endl;
+}
