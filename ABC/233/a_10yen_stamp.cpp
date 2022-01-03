0db3dde3d01aa6227f89ee360c831194e8c408c7
MK-nn <okuken99@gmail.com>
2022-01-03 19:08:58 +0900

    solve abc 233 a

---
+++ ABC/233/a_10yen_stamp.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x, y;
+  cin >> x >> y;
+
+  int z = y - x;
+  if (z <= 0)
+  {
+    cout << 0 << endl;
+  }
+  else
+  {
+    if (z % 10 == 0)
+    {
+      cout << z / 10 << endl;
+    }
+    else
+    {
+      cout << z / 10 + 1 << endl;
+    }
+  }
+
+  return 0;
+}
